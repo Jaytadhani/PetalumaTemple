8453cc487b72fe2af5bd0a1c1cf0b67a1987bf85
aftabmashakpautra <166187294+aftabmashakpautra@users.noreply.github.com>
2025-01-21 12:04:51 +0530

    navbar layout Logo

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,23 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
+import Contact from "./Pages/Contact";
+import About from "./Pages/About";
 
 
 const App = () => {
   return (
     <Router>
-        <Navbar />
+      
       <div>
         {/* Define Routes */}
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact/>} />
+          <Route path="/about" element={<About/>} />
         </Routes>
       </div>
-      <Footer/>
+    
     </Router>
   );
 };
